6681895a6e65ada05bb8c2ebc8f9936a1fb72685
555032 <charlesprindle@new-host.home>
2017-05-30 15:23:24 -0400

    refactored app.js for angular

--- public/app.js
+++ public/app.js
@@ -4,60 +4,70 @@ app.controller('mainController', function($http, $scope, $localStorage){
   $scope.$storage = $localStorage.$default({
     aboutModal: false,
     players: [],
-    QBs: [],
-    WRs: [],
-    RBs: [],
-    TEs: [],
-    Ds: [],
     showPlayers: 'QB',
     sortby: '-projection',
     lastSort: '-projection',
     sortAsc: false,
     sortCategory: 'projection',
-    removedPlayers: []
+    removedPlayers: [],
+    times: [],
+    teams: []
   });
 
   this.setShow = function(position){
     $localStorage.showPlayers = position;
   };
 
+  this.removeTeam = function(name){
+    for(let i=$localStorage.players.length-1; i>=0; i--){
+      if($localStorage.players[i].team==name){
+        this.removePlayer($localStorage.players[i], $localStorage.players[i].Position);
+      }
+    }
+  };
+
+  this.replaceTeam = function(name){
+    for(let i=$localStorage.removedPlayers.length-1; i>=0; i--){
+      if($localStorage.removedPlayers[i].team==name){
+        this.addPlayer($localStorage.removedPlayers[i]);
+      }
+    }
+  };
+
+  this.removeTime = function(time){
+    for(let i=$localStorage.players.length-1; i>=0; i--){
+      if($localStorage.players[i].gameTime==time){
+        this.removePlayer($localStorage.players[i], $localStorage.players[i].Position);
+      }
+    }
+  };
+
+  this.replaceTime = function(time){
+    for(let i=$localStorage.removedPlayers.length-1; i>=0; i--){
+      if($localStorage.removedPlayers[i].gameTime==time){
+        this.addPlayer($localStorage.removedPlayers[i]);
+      }
+    }
+  };
+  $scope.toggleX = function (x) {
+    x.toggled = !x.toggled;
+  };
+
   this.removePlayer = function(player, position){
-    console.log(player);
+    console.log(position);
     $localStorage.removedPlayers.push(player);
     function findPlayer(p){
       return p.id == player.id;
     }
-    if(position == 'QB'){
-      $localStorage.QBs.splice($localStorage.QBs.indexOf($localStorage.QBs.find(findPlayer)),1);
-    }else if (position == 'RB') {
-      $localStorage.RBs.splice($localStorage.RBs.indexOf($localStorage.RBs.find(findPlayer)),1);
-    }else if (position == 'WR') {
-      $localStorage.WRs.splice($localStorage.WRs.indexOf($localStorage.WRs.find(findPlayer)),1);
-    }else if (position == 'TE') {
-      $localStorage.TEs.splice($localStorage.TEs.indexOf($localStorage.TEs.find(findPlayer)),1);
-    }else if (position == 'D') {
-      $localStorage.Ds.splice($localStorage.Ds.indexOf($localStorage.Ds.find(findPlayer)),1);
-    }
-    console.log($localStorage.removedPlayers);
+    $localStorage.players.splice($localStorage.players.indexOf($localStorage.players.find(findPlayer)),1);
   };
 
   this.addPlayer = function(player){
-    console.log(player);
     function findPlayer(p){
       return p.id == player.id;
     }
     $localStorage.removedPlayers.splice($localStorage.removedPlayers.indexOf($localStorage.removedPlayers.find(findPlayer)),1);
-    if(player.Position == 'QB'){
-      $localStorage.QBs.push(player);
-    }else if (player.Position == 'RB') {
-      $localStorage.RBs.push(player);
-    }else if (player.Position == 'WR') {
-      $localStorage.WRs.push(player);
-    }else if (player.Position == 'TE') {
-      $localStorage.TEs.push(player);
-    }else if (player.Position == 'DST') {
-      $localStorage.Ds.push(player);
-    }
+      $localStorage.players.push(player);
   };
 
   this.sortTableBy = function(sort){
@@ -78,37 +88,52 @@ app.controller('mainController', function($http, $scope, $localStorage){
     method: 'GET',
     url: 'http://localhost:3000/players',
   }).then(function(response){
-    console.log($localStorage.players.length);
     if($localStorage.players.length !== 0){
-      console.log("in local already");
-      //need to update local
+      //check for updates
     }else{
-    $localStorage.players = response.data;
-    console.log($localStorage.players);
-    $localStorage.players.forEach(function(player){
-      if(player.projection !== null){
-        player.projection = parseFloat(player.projection);
-      }else{
-        player.projection = 0;
-      }
-      if(player.Value !== null){
-        player.Value = parseFloat(player.Value);
-      }else{
-        player.Value = 0;
-      }
-      if(player.Position === 'QB'){
-        $localStorage.QBs.push(player);
-      }else if (player.Position === 'WR') {
-        $localStorage.WRs.push(player);
-      }else if (player.Position === 'RB') {
-        $localStorage.RBs.push(player);
-      }else if (player.Position === 'TE') {
-        $localStorage.TEs.push(player);
-      }else if (player.Position === 'DST') {
-        $localStorage.Ds.push(player);
-      }
-    });
-  }
+      $localStorage.players = response.data;
+      $localStorage.players.forEach(function(player){
+        let timeFound = false;
+        if($localStorage.times.length == 0){
+          newTime = {time: player.gameTime, toggled: false}
+          $localStorage.times.push(newTime);
+        }else{
+          for(let i = 0; i<$localStorage.times.length; i++){
+            if($localStorage.times[i].time===player.gameTime){
+              timeFound = true;
+            }
+          }
+          if(!timeFound){
+            newTime = {time: player.gameTime, toggled: false};
+            $localStorage.times.push(newTime);          }
+        }
+        let teamFound = false;
+        if($localStorage.teams.length == 0){
+          newTeam = {name: player.team, toggled: false}
+          $localStorage.teams.push(newTeam);
+        }else{
+          for(let i = 0; i<$localStorage.teams.length; i++){
+            if($localStorage.teams[i].name===player.team){
+              teamFound = true;
+            }
+          }
+          if(!teamFound){
+            newTeam = {name: player.team, toggled: false}
+            $localStorage.teams.push(newTeam);
+         }
+        }
+        if(player.projection !== null){
+          player.projection = parseFloat(player.projection);
+        }else{
+          player.projection = 0;
+        }
+        if(player.Value !== null){
+          player.Value = parseFloat(player.Value);
+        }else{
+          player.Value = 0;
+        }
+      });
+    }
   });
-
+  $localStorage.teams.sort();
 });
